0cd44dbb7ebfae872d055c694735f86758abc7e5
daijunliang <daijl09@chinatelecom.cn>
2024-06-14 15:27:38 +0800

    feat:添加_config.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'docs',
-    basePath: '/docs',
     output: 'export',
     webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
         // Perform customizations to webpack config
